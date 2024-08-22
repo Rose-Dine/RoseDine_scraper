dd0162e8af26d5dd9c5e4ef8acc8859578d323d0
rhit-srivasa <srivasa@rose-hulman.edu>
2024-08-23 02:23:02 +0530

    Initial Push [Web_Scraper]

---
+++ main.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+import schedule
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+def run_schedule():
+    chromedriver_path = 'PyScraping\\chromedriver-win64\\chromedriver.exe'
+    service = Service(executable_path=chromedriver_path)
+    driver = webdriver.Chrome(service=service)
+    with open('PyScraping\\output.txt', 'a') as file:
+        schedule.scrape_weekly_meals(driver, file)
+    driver.quit()
+
+def run_api_jsonify():
+    subprocess.run([sys.executable, "PyScraping\\api_jsonify.py"])
+
+if __name__ == "__main__":
+    run_schedule()
+    print("schedule.py finished. Running api_jsonify.py...")
+    run_api_jsonify()
+    print("All tasks completed.")
